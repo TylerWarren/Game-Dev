8f5f93f6a0c162dd014a129e8c93abdf317d0891
Tyler Warren <tylerwarren345@gmail.com>
2024-09-01 14:57:29 -0600

    Added a mono events behavior script and added two new unity events.

--- My project/Assets/Scenes/FloatData.cs
+++ My project/Assets/Scenes/FloatData.cs
@@ -1,9 +1,15 @@
+using System.Globalization;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject 
 {   
     public float value;
+
+    public void SetValue(float num)
+    {
+        value = num;
+    }
 
     public void UpdateValue(float num)
     {
