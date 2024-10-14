4a424335e580c706d2d8a2ae259a78551b540acb
Tyler Warren <tylerwarren345@gmail.com>
2024-10-13 19:52:57 -0600

    Added a lava pit and extended the level with vertical platforms and a wall.

---
+++ Platform_Game/Assets/Scenes/Scripts/DestroyOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    public float holdTime = 0.1f;
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            yield return new WaitForSeconds(holdTime);
+            
+         
+            if (other != null)
+            {
+                Destroy(other.gameObject); 
+            }
+        }
+    }
+}
