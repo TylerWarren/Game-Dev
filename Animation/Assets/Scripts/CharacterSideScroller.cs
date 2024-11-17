858456072328d6684c6da57c500cfb91736cff30
Tyler Warren <tylerwarren345@gmail.com>
2024-11-17 14:14:45 -0700

    Made some final adjustments to the animations and transitions.

--- Animation/Assets/Scripts/CharacterSideScroller.cs
+++ Animation/Assets/Scripts/CharacterSideScroller.cs
@@ -16,6 +16,8 @@ public class CharacterSideScroller : MonoBehaviour
 
     public UnityEvent jumpEvent;
     public UnityEvent runEvent;
+    public UnityEvent idleEvent;
+    public UnityEvent jumpEndEvent;
 
     private void Start()
     {
@@ -23,9 +25,10 @@ public class CharacterSideScroller : MonoBehaviour
         animator = GetComponent<Animator>();
         jumpsRemaining = maxJumps;
 
-        // Assign listeners
         jumpEvent.AddListener(() => animator.SetTrigger("Jump"));
-        runEvent.AddListener(() => animator.SetBool("IsRunning", isMoving));
+        runEvent.AddListener(() => animator.SetBool("IsRunning", true));
+        idleEvent.AddListener(() => animator.SetBool("IsRunning", false));
+        jumpEndEvent.AddListener(() => animator.SetTrigger("Land")); // Transition to idle
     }
 
     private void Update()
@@ -56,7 +59,7 @@ public class CharacterSideScroller : MonoBehaviour
             if (isMoving)
             {
                 isMoving = false;
-                runEvent.Invoke();
+                idleEvent.Invoke();
             }
         }
     }
@@ -66,17 +69,19 @@ public class CharacterSideScroller : MonoBehaviour
         if (!controller.isGrounded)
         {
             velocity.y += gravity * Time.deltaTime;
+            animator.SetBool("IsGrounded", false);
         }
         else
         {
             velocity.y = 0;
             jumpsRemaining = maxJumps;
+            animator.SetBool("IsGrounded", true);
         }
     }
 
     private void Jump()
     {
-        if (!Input.GetButton("Jump") || (!controller.isGrounded && jumpsRemaining <= 0)) return;
+        if (!Input.GetButtonDown("Jump") || jumpsRemaining <= 0) return;
 
         velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
         jumpsRemaining--;
